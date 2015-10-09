d15e5611a5b57c1d35bad1616a9daf595bacd1a6
Taova <taomarina@yandex.ru>
2015-10-09 12:29:44 +0400

    9_10 master_popup

--- app/js/app_progect.js
+++ app/js/app_progect.js
@@ -3,9 +3,19 @@ var myModule=(function(){
 	var init = function(){
 		_setUpListners();
 	};
-	var _setUpListners = function(){
 
-	}
+	var _setUpListners = function(){
+		$('#add-new-item').on('click', _showModal);//открыть модальное окно
+		};
+
+	var _showModal = function (ev) {
+		ev.preventDefault();
+		$("#new-project-popup").bPopup({
+			speed:650,
+			transition: "slideDown"
+		});
+		console.log("fdfdd");
+	};
 	return{
 		init:init
 	};
